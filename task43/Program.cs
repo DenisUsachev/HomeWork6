ffb5c98230859f5db721b57a73b3264dd3517426
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-18 11:43:04 +0500

    Fix commits

--- task43/Program.cs
+++ task43/Program.cs
@@ -16,4 +16,4 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 double x = (-b2 + b1) / (-k1 + k2);
 double y = k2 * x + b2;
 
-Console.WriteLine($"Точкой пересечения двух прямых будет равна: X-{x}, Y-{y}");
+Console.WriteLine($"Точкой пересечения двух прямых будет равна: X: {x}, Y: {y}");
