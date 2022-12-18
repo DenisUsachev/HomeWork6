a5892475c95a4d3e3fec2eabffff6f7a0760c83e
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-18 12:02:35 +0500

    Add commits

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.Write("Сколько количество чисел в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [number];
+
+    
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"Введите {i} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        count++;
+    }
+}
+
+
+Console.WriteLine($"Количество положительных чисел является: {count}");
